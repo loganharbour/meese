808535d64e9f1903338971d6535048e46188f923
Logan Harbour <loganhharbour@gmail.com>
2024-06-05 15:17:41 -0600

    Initial commit of Material source for k

---
+++ src/materials/LaplaceYoungK.C
@@ -0,0 +1,25 @@
+#include "LaplaceYoungK.h"
+
+registerMooseObject("MeeseApp", LaplaceYoungK);
+
+InputParameters
+LaplaceYoungK::validParams()
+{
+  InputParameters params = Material::validParams();
+  params.addParam<MaterialPropertyName>("k", "k", "The name of the property for k");
+  params.addCoupledVar("u", "u", "The variable to take the gradient of");
+  return params;
+}
+
+LaplaceYoungK::LaplaceYoungK(const InputParameters & parameters)
+  : Material(parameters),
+    _k(declareADProperty<Real>(getParam<MaterialPropertyName>("k"))),
+    _grad_u(adCoupledGradient("u"))
+{
+}
+
+void
+LaplaceYoungK::computeQpProperties()
+{
+  _k[_qp] = 0;
+}
